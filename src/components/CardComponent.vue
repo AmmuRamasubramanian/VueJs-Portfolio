ba7c62b477a003d920aba338b03acdcc6eee5aab
sivagnana sowndarya <sowndarya@sowndarya-MacBook-Pro.local>
2026-04-04 19:53:41 +0530

    changes for card component-8

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -3,6 +3,9 @@ import Blog from '../assets/blog.svg'
 import Instagram from '../assets/instagram.svg'
 import Linkedin from '../assets/linkedin.svg'
 import Fire from '../assets/fire.svg'
+import { useRouter } from 'vue-router'
+
+const router=useRouter()
 
 const handleOpenLinkedIn=()=>{
   window.open('https://www.linkedin.com/in/sivagnanasowndaryar/', '_blank')
@@ -16,62 +19,96 @@ const handleOpenBlogs=()=>{
   window.open('https://medium.com/@sivagnanasowndarya', '_blank')
 }
 
+const handleNavigateExploreshad=()=>{
+  router.push('/shadcncomponents')
+}
+
 </script>
 
 <template>
   <div class="carddiv">
-    <div class="carddiv__curve">
-      <svg viewBox="0 0 200 200">
-          <path
-            d="M10 100 Q 100 180 180 100"
-            fill="none"
-            stroke="var(--redColor_2)"
-            stroke-width="3"
-            stroke-dasharray="8 8"
-          />
-        </svg>
-      <div class="carddiv__fireOuter">
-        <Fire class="carddiv__fireIcon"/>
+    <div class="carddiv__innercarddiv">
+      <div class="carddiv__curve">
+        <svg viewBox="0 0 200 200">
+            <path
+              d="M10 100 Q 100 180 180 100"
+              fill="none"
+              stroke="var(--redColor_2)"
+              stroke-width="3"
+              stroke-dasharray="8 8"
+            />
+          </svg>
+        <div class="carddiv__fireOuter">
+          <Fire class="carddiv__fireIcon"/>
+        </div>
+      </div>
+      <div class="carddiv__imgouter">
+      <img src="../assets/sowndImg.jpeg" class="carddiv__profileImg"/>
+      <p class="carddiv__name">Sowndarya R</p>
+      </div>
+      <div>
+          <p class="carddiv__subtext">A frontend software developer who enjoys solving new challenges every day.</p>
+          <div class="carddiv__footercontainer">
+            <div class="carddiv__footericonouter" @click="handleOpenBlogs">
+              <Blog class="carddiv__footericon"/>
+            </div>
+            <div class="carddiv__footergap"/>
+            <div class="carddiv__footericonouter" @click="handleOpenInsta">
+              <Instagram class="carddiv__footericon"/>
+            </div>
+            <div class="carddiv__footergap"/>
+            <div class="carddiv__footericonouter" @click="handleOpenLinkedIn">
+              <Linkedin class="carddiv__footericon"/>
+            </div>
+          </div>
       </div>
     </div>
-    <div class="carddiv__imgouter">
-     <img src="../assets/sowndImg.jpeg" class="carddiv__profileImg"/>
-     <p class="carddiv__name">Sowndarya R</p>
-    </div>
-    <div>
-        <p class="carddiv__subtext">A frontend software developer who enjoys solving new challenges every day.</p>
-        <div class="carddiv__footercontainer">
-          <div class="carddiv__footericonouter" @click="handleOpenBlogs">
-            <Blog class="carddiv__footericon"/>
-          </div>
-          <div class="carddiv__footergap"/>
-          <div class="carddiv__footericonouter" @click="handleOpenInsta">
-            <Instagram class="carddiv__footericon"/>
-          </div>
-          <div class="carddiv__footergap"/>
-          <div class="carddiv__footericonouter" @click="handleOpenLinkedIn">
-            <Linkedin class="carddiv__footericon"/>
-          </div>
-        </div>
+    <div class="carddiv__explorediv cursordiv" @click="handleNavigateExploreshad">
+      <p class="carddiv__exploretext">Explore My ShadCN Components</p>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
   .carddiv{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: white;
-    height: 470px;
-    width: 260px;
-    border-radius: 6px;
-    padding:25px 15px;
-    justify-content: space-between;
-    position: relative;
-    overflow: hidden;
-    margin-top:2%;
     position: fixed;
+    display:flex;
+    flex-direction: column;
+
+    &__innercarddiv{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      background-color: white;
+      height: 470px;
+      width: 260px;
+      border-radius: 6px;
+      padding:25px 15px;
+      justify-content: space-between;
+      position: relative;
+      overflow: hidden;
+      margin-top:2%;
+    }
+
+    &__explorediv{
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      height: 36px;
+      min-height: 36px;
+      background-color: white;
+      margin-top:10px;
+      width: 260px;
+      border-radius: 10px;
+    }
+
+    &__exploretext{
+      color: var(--redColor);
+      font-family: var(--poppins);
+      font-size: 0.8em;
+      font-weight: 700;
+    }
 
     &__imgouter{
         display: flex;
